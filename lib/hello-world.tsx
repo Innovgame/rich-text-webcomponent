7c28ec9a7b432e11c1ce94b79da5030fb26aeb47
Innovgame <Innovgame@163.com>
2025-10-21 18:36:29 +0800

    build react component

---
+++ lib/hello-world.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HelloWebComponent: React.FC = () => {
+    return <div>Hello Web Commponent.</div>;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true, outDir: "./dist" })],
-  build: {
-    lib: {
-      entry: "./lib/entry.ts",
-      name: "RichTextWebComponent",
-      fileName: "rich-text-webcomponent",
+    plugins: [react(), dts({ rollupTypes: true, outDir: "./dist" })],
+    build: {
+        lib: {
+            entry: "./lib/entry.ts",
+            name: "RichTextWebComponent",
+            fileName: "rich-text-webcomponent",
+        },
     },
-  },
 });
